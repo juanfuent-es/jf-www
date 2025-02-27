c3328e6d21ede7cf3da0f1c0788f337d3f60fa51
JuanFuent.es <input@juanfuent.es>
2025-02-26 23:48:24 -0600

    Add footer links

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("Hola mundo")
+export default class VariableWord {
+    constructor(_item) {
+        this.childs = _item.querySelector('span')
+        this.childs.array.forEach(child => child.setupVar(_item))
+    }
+
+    setupVar(_item) {
+
+    }
+}
